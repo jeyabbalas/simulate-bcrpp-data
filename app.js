bce94a4e826fe1a83cf0b847543da16b7c2c7768
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2024-03-14 11:02:54 -0400

    Implement BPC3 simulation

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-import {generateSimulatedICARELitData} from './lib/main.js';
+import {generateSimulatedBPC3Data} from './lib/main.js';
 
 
-const simulatedData = await generateSimulatedICARELitData({n: 5});
-console.log(simulatedData);
+const simulatedData = await generateSimulatedBPC3Data({n: 5000});
+//const simulatedData = await generateSimulatedBPC3Data({n: 100, caseControl: true});
+//console.log(simulatedData);
+console.log(simulatedData.filter(item => item.observed_outcome === 1).length);
