c1dce4db8ab7ff47737de7170b24ecf2a68ca837
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2024-03-14 15:53:22 -0400

    Demonstrate data analysis

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ import {fetchFileAsText, fetchFileAsJson} from './lib/utils.js';
 import {fitLogisticRegression} from './lib/stats.js';
 
 
-const simulatedData = await generateSimulatedBPC3Data({n: 100});
-//const simulatedData = await generateSimulatedBPC3Data({n: 100, caseControl: true});
+//const simulatedData = await generateSimulatedBPC3Data({n: 25000});
+const simulatedData = await generateSimulatedBPC3Data({n: 1000, caseControl: true});
 console.log(simulatedData);
 
 // BPC3 parameters
-// const rootUrl = "https://raw.githubusercontent.com/jeyabbalas/simulate-bcrpp-data/main/data/bpc3/";
-// const formulaUrl = rootUrl + "model_formula.txt";
-// const dtypesUrl = rootUrl + "dtypes.json";
-//
-// const formula = "observed_outcome ~ study_entry_age + " + (await fetchFileAsText(formulaUrl));
-// const dtypes = await fetchFileAsJson(dtypesUrl);
-//
-// const modelOutput = await fitLogisticRegression(formula, JSON.stringify(simulatedData), JSON.stringify(dtypes));
-// console.log(modelOutput);
+const rootUrl = "https://raw.githubusercontent.com/jeyabbalas/simulate-bcrpp-data/main/data/bpc3/";
+const formulaUrl = rootUrl + "model_formula.txt";
+const dtypesUrl = rootUrl + "dtypes.json";
+
+const formula = "observed_outcome ~ study_entry_age + " + (await fetchFileAsText(formulaUrl));
+const dtypes = await fetchFileAsJson(dtypesUrl);
+
+const modelOutput = await fitLogisticRegression(formula, JSON.stringify(simulatedData), JSON.stringify(dtypes));
+console.log(modelOutput);
