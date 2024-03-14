74c9057e4d7564d43550d6db05ecae24d503b246
Jeya Balaji Balasubramanian <jeyabbalas@gmail.com>
2024-03-14 11:11:25 -0400

    Remove iCARE-Lit

--- app.js
+++ app.js
@@ -3,5 +3,4 @@ import {generateSimulatedBPC3Data} from './lib/main.js';
 
 const simulatedData = await generateSimulatedBPC3Data({n: 5000});
 //const simulatedData = await generateSimulatedBPC3Data({n: 100, caseControl: true});
-//console.log(simulatedData);
-console.log(simulatedData.filter(item => item.observed_outcome === 1).length);
+console.log(simulatedData);
